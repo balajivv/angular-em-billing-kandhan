d903e506fc1c4f5605c91700bc63084dcf2e60cb
balajivv <balajivv@outlook.com>
2021-03-07 23:43:06 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, NgMultiSelectDropDownModule.forRoot()],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent]
 })
